0734a1976baab87cc8366d9b4ddae707bc7a1a87
Haaris Ahmad <haaris.ahmad@mslsolutions.com>
2025-09-26 16:23:55 +1000

    added implementation for getArnieQuotes

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,22 @@
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  var responses = await Promise.all(
+    urls.map(async (url) => {
+      var successKey = 'Arnie Quote';
+      var failureKey = 'FAILURE';
+      const res = await httpGet(url);
+      const { message } = JSON.parse(res.body);
+
+      if (res.status === 200) {
+        return { [successKey]: message };
+      } else {
+        return { [failureKey]: message };
+      }
+    })
+  );
+
+  return responses;
 };
 
 module.exports = {
